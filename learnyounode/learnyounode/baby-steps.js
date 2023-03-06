b1ad60478dafce00aa0a548fde76450b5307f0dd
lukinhaxx <126528237+lukinhaxx@users.noreply.github.com>
2023-03-06 20:49:19 -0300

    Add files via upload

---
+++ learnyounode/learnyounode/baby-steps.js
@@ -0,0 +1,2 @@
+console.log(Number(process.argv[2])+Number(process.argv[3])+Number(process.argv[4]))
+
---
+++ learnyounode/learnyounode/http-uppercaserer.js
@@ -0,0 +1,20 @@
+//É necessário instalar antes => npm install through2-map
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString().toUpperCase();
+    });
+    req.on('end', () => {
+      res.end(body);
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(process.argv[2]);
